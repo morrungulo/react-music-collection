3a0977bb3cd09a7793437e8025952e924612c458
Alexandre Lopes <morrungulo@gmail.com>
2022-06-08 14:40:33 +0000

    change from promises to async/await

--- server/controllers/music.js
+++ server/controllers/music.js
@@ -22,52 +22,58 @@ const resultNotOk = (message) => {
   }
 }
 
-const get_all_albums = (req, res, next) => {
-  Album.find()
-    .then(albums => {
-      res.status(200).json(resultOk(albums))
-    })
-    .catch(err => next(err))
+const get_all_albums = async (req, res, next) => {
+  try {
+    const albums = await Album.find()
+    res.status(200).json(resultOk(albums))
+  } catch (error) {
+    next(error)
+  }
 }
 
-const get_album = (req, res, next) => {
+const get_album = async (req, res, next) => {
   const { id } = req.params
-  Album.findById(id)
-    .then(album => {
-      const result = album === null ? resultNotOk("unknown album") : resultOk(album)
-      res.status(200).json(result)
-    })
-    .catch(err => next(new AlbumNotFoundError()))
+  try {
+    const album = await Album.findById(id)
+    const result = (album === null) ? resultNotOk("unknown album") : resultOk(album)
+    res.status(200).json(result)
+  } catch (error) {
+    next(new AlbumNotFoundError())
+  }
 }
 
-const post_add_album = (req, res, next) => {
+const post_add_album = async (req, res, next) => {
   const body = _.pick(req.body, ['name', 'artist', 'image', 'songs', 'rating', 'favorite'])
-  Album.create(body)
-    .then(album => {
-      res.status(201).json(req.minimal ? resultOkNoData() : resultOk(album))
-    })
-    .catch(err => next(new AlbumValidationError(err.message)))
+  try {
+    const album = await Album.create(body)
+    const result = req.minimal ? resultOkNoData() : resultOk(album)
+    res.status(201).json(result)
+  } catch (error) {
+    next(new AlbumValidationError(err.message))
+  }
 }
 
-const delete_album = (req, res, next) => {
+const delete_album = async (req, res, next) => {
   const { id } = req.params;
-  Album.findByIdAndDelete(id)
-    .then(album => {
-      const result = album === null ? resultNotOk("unknown album") : resultOkNoData()
-      res.status(200).json(result)
-    })
-    .catch(err => next(new AlbumNotFoundError()))
+  try {
+    const album = await Album.findByIdAndDelete(id)
+    const result = album === null ? resultNotOk("unknown album") : resultOkNoData()
+    res.status(200).json(result)
+  } catch (error) {
+    next(new AlbumNotFoundError())
+  }
 }
 
-const patch_album = (req, res, next) => {
+const patch_album = async (req, res, next) => {
   const { id } = req.params;
   const body = _.pick(req.body, ['name', 'artist', 'image', 'rating', 'favorite'])
-  Album.findByIdAndUpdate(id, { $set: body }, { new: !req.minimal })
-    .then(album => {
-      const result = album === null ? resultNotOk("unknown album") : req.minimal ? resultOkNoData() : resultOk(album)
-      res.status(200).json(result)
-    })
-    .catch(err => next(new AlbumNotFoundError()))
+  try {
+    const album = await Album.findByIdAndUpdate(id, { $set: body }, { new: !req.minimal })
+    const result = album === null ? resultNotOk("unknown album") : req.minimal ? resultOkNoData() : resultOk(album)
+    res.status(200).json(result)
+  } catch (error) {
+    next(new AlbumNotFoundError())
+  }
 }
 
 module.exports = {
